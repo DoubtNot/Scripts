dbc777e2a4fed2df7a19dcf883cd216d8f0ca725
DoubtNot <lastof6.boys@gmail.com>
2023-11-12 20:22:33 -0700

    Note Changes To Scripts

--- PythonScripts/RandomPlacement.py
+++ PythonScripts/RandomPlacement.py
@@ -6,15 +6,15 @@ def placement_generator(x_min, x_max, y_min, y_max, z_min, z_max, num_dups):
 
     for obj in selected_objects:
         for _ in range(num_dups):
-            # Duplicate current object
+
             duplicate_obj = cmds.duplicate(obj, rr=True)[0]
 
-            # Generate new x, y, z values based on range arguments
             rand_x = random.uniform(x_min, x_max)
             rand_y = random.uniform(y_min, y_max)
             rand_z = random.uniform(z_min, z_max)
 
-            # Translate object to the new location
             cmds.xform(duplicate_obj, worldSpace=True, translation=(rand_x, rand_y, rand_z))
 
+# How To Use: Change the numbers to set a random location
+# Number Meanings In Order: x_min, x_max, y_min, y_max, z_min, z_max, num_dups
 placement_generator(-10, 10, -20, 40, -10, 10, 5)
